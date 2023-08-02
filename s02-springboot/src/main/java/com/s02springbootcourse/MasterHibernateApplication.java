88efecdd95d58286f17fd61735155f34f08951d6
taras.chmeruk <taras.chmeruk@gameloft.com>
2023-08-02 10:27:50 +0300

    first commit

---
+++ s02-springboot/src/main/java/com/s02springbootcourse/MasterHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.s02springbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterHibernateApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (MasterHibernateApplication.class, args);
+    }
+
+}
---
+++ s02-springboot/src/test/java/com/s02springbootcourse/MasterHibernateApplicationTest.java
@@ -0,0 +1,15 @@
+package com.s02springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class MasterHibernateApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
