6868e3fea8211e06d8c7ddaf178f3cd8033e27fa
taras.chmeruk <taras.chmeruk@gameloft.com>
2023-08-02 13:17:17 +0300

    s03

---
+++ s03-jdbc-to-jpa/src/main/java/com/s03sdbctojpa/JdbcToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.s03sdbctojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcToJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcToJpaApplication.class, args);
+	}
+
+}
